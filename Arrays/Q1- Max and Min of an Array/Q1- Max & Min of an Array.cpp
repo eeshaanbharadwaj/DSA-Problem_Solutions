8099eee2bad2efdc6fae304459b5fff50e425d9e
eeshaanbharadwaj <1by22is053@bmsit.in>
2025-09-06 19:40:55 +0530

    done

--- Arrays/Q1- Max and Min of an Array/Q1- Max & Min of an Array.cpp
+++ Arrays/Q1- Max and Min of an Array/Q1- Max & Min of an Array.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int main(){
     int arr[5] = {4,3,6,1,-1};
     int size = sizeof(arr)/sizeof(int);
-
     int largest = INT_MIN;
     int smallest = INT_MAX;
     for(int i=0; i<size; i++){
