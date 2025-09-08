6e058dfd75eb748db19fb96bdfaad1e85f33b22e
eeshaanbharadwaj <1by22is053@bmsit.in>
2025-09-08 18:38:33 +0530

    done

---
+++ Arrays/Q5- Kadane's Algorithm/KA.cpp
@@ -0,0 +1,17 @@
+// leetcode problem
+//Maximum SubArray
+class Solution {
+    public:
+        int maxSubArray(vector<int>& nums) {
+            int maxsum = INT_MIN;
+            int currsum = 0;
+            for(int val: nums){
+                currsum = currsum + val;
+                maxsum = max(currsum, maxsum);
+                if(currsum<0){
+                    currsum = 0;
+                }
+            }
+            return maxsum;a
+        }
+    };
