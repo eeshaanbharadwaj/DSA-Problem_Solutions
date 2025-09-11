f05d1eeb31a682ef5dfc87ec80875634b6e6820f
eeshaanbharadwaj <1by22is053@bmsit.in>
2025-09-11 06:27:55 +0530

    done

---
+++ Arrays/Q10- Max water stored in a Container/max.cpp
@@ -0,0 +1,18 @@
+//leetcode problem
+int maxArea(vector<int>& height) {
+    int i = 0, j = height.size() - 1;
+    int maxArea = 0;
+
+    while (i < j) {
+        int h = min(height[i], height[j]);
+        int w = j - i;
+        maxArea = max(maxArea, h * w);
+
+        if (height[i] < height[j]) {
+            i++;
+        } else {
+            j--;
+        }
+    }
+    return maxArea;
+}
